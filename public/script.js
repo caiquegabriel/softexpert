c62e3a2a129f77780df970a619adb042b68dc411
“Caique” <cgabrieladm@icloud.com>
2022-01-16 17:20:40 -0300

    frontend, Tela Venda

--- public/script.js
+++ public/script.js
@@ -1,3 +1,19 @@
 $(function(){
-   
+    $('[produtos]').each(function(){
+        let $produtos = $(this); 
+        $(document).on('click', '[add]', function(){
+            $produtos.append($(this).parents('[produto]').clone());
+        });  
+    });  
+
+    $(document).find('[produtos] select').change(function(){
+        $(document).find('[produtos] select option:selected').each(function(){
+            alert("sim");
+        });
+    });
+
+
+
+
+
 });
--- views/view_register_order_form.php
+++ views/view_register_order_form.php
@@ -9,9 +9,10 @@
         <link rel='stylesheet' id='fontAwesome-style-2-css'  href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css?ver=2.1.3' type='text/css' media='' />    
         <link rel="stylesheet" href="../public/style.css" /> 
     
-        <script src="../public/script.js"></script>
+         
         <script src="../public/jquery.js" ></script> 
         <script src="../public/jquery_masks.js" ></script> 
+        <script src="../public/script.js"></script>
     </head>
     
      
@@ -37,14 +38,31 @@
 
                                 <form> 
 
-                                    <div> 
-                                        <select name="product[]">
-                                            <?php foreach( $products as $product ): ?>
-                                                <option value="<?= $product['id'] ?>"><?= $product['nome'] ?> ( <?= mask_price( $product['preco_unidade'] , true) ?> + <?= mask_price( $product['produto_percentual_imposto'] ) ?>% ) </option>
-                                            <?php endforeach; ?>
-                                        </select>
-                                        <input>
+                                    <div produtos> 
+                                        <div produto class="row"> 
+                                            <div class="col-7"> 
+                                                <select name="product[]">
+                                                    <?php foreach( $products as $product ): ?>
+                                                        <option value="<?= $product['id'] ?>" percentual="<?= $product['produto_percentual_imposto'] ?>" valor="<?= $product['preco_unidade'] ?>" ><?= $product['nome'] ?> ( <?= mask_price( $product['preco_unidade'] , true) ?> + <?= mask_price( $product['produto_percentual_imposto'] ) ?>% ) </option>
+                                                    <?php endforeach; ?>
+                                                </select>
+                                            </div>
+                                            <div class="col-4"> 
+                                                <input />
+                                            </div> 
+                                            <div class="col-1"> 
+                                                <a href="#" add>Adicionar mais</a> 
+                                            </div> 
+                                        </div>  
                                     </div>
+                                     
+
+                                    <!-- Informações da compra -->
+                                    <div> 
+                                        <p>Preço total: </p>
+                                        <p>Preço total com impostos: </p>
+                                        <p>Produtos selecionados: </p>
+                                    </div> 
 
                                 </form>
                                 
