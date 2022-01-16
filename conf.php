c2770e56dca993992ddbcecdba6000c679750478
“Caique” <cgabrieladm@icloud.com>
2022-01-16 20:39:28 -0300

    conf.php

--- conf.php
+++ conf.php
@@ -6,6 +6,12 @@
     /*
         Configurações da base de dados 
     */
+
+    const DB_USER = "postgres";
+    const DB_USER_PASS = "123post";
+    const DB_HOST = "localhost";
+    const DB_PORT = "5432";
+    const DB_NAME = "loja";
     
 
 
--- core/db.php
+++ core/db.php
@@ -2,8 +2,8 @@
 
     function db(){
         try{ 
-            $dsn = "pgsql:host=localhost;port=5432;dbname=loja;";
-            $conn  = new \PDO($dsn, "postgres", "123post",
+            $dsn = "pgsql:host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";";
+            $conn  = new \PDO($dsn, DB_USER, DB_USER_PASS,
                 array(
                     \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
                     \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
@@ -13,7 +13,7 @@
                 )
             );
         }catch( Exception $e ){ 
-            die("Houve um erro crítico. <br> <strong>Código do Erro:</strong> #{$e->getCode()}");
+            die("Houve um erro crítico. <br> <strong>Código do Erro:</strong> #{$e->getCode()}<br><br> Mensagem: " . $e->getMessage());
         }
 
         return $conn;
